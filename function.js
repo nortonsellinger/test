66c92d0d1fdb652395fc43b6c571f6c239ddf277
Norton Sellinger <47449946+nortonsellinger@users.noreply.github.com>
2023-03-25 15:57:51 +0800

    upd

--- function.js
+++ function.js
@@ -1,3 +1,10 @@
-document.getElementById('button').addEventListener('click', function() {
-    alert("нихуя ты молодец!");
+
+
+const device_width = window.screen.width;
+const device_height = window.screen.height;
+const device_ratio = device_width / device_height;
+document.documentElement.style.setProperty('--DEVICE_RATIO', 'device_ratio');
+
+document.getElementById('button').addEventListener('click', function() { //кнопочька
+    alert(device_ratio);
 });
