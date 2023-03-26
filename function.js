c9d6ea0914dd695df7790cf5f84f4ad82c2c342c
Norton Sellinger <47449946+nortonsellinger@users.noreply.github.com>
2023-03-26 21:19:45 +0800

    Update function.js

--- function.js
+++ function.js
@@ -1,14 +1,14 @@
 //ПОЛУЧАЕМ ШИРИНУ ЭКРАНА
-const device_width = window.screen.width;
+const dvc_width = window.screen.width;
 //ПОЛУЧАЕМ ВЫСОТУ ЭКРАНА
-const device_height = window.screen.height;
+const dvc_height = window.screen.height;
 //ВЫЧИСЛЯЕМ ОТНОШЕНИЕ !ШИРИНЫ! К !ВЫСОТЕ!
-const device_ratio = device_width / device_height;
+const dvc_ratio = dvc_width / dvc_height;
 //ПРИСВАИВАЕМ ПЕРЕМЕННОЙ --DEVICE_RATIO В CSS ВЫЧИСЛЕННОЕ СООТНОШЕНИЕ СТОРОН УСТРОЙСТВА
 document.documentElement.style.setProperty('--DEVICE_RATIO', 'device_ratio');
 
 function alertDeviceRatio() {
-    alert(Math.round((device_ratio + Number.EPSILON) * 100) / 100);
+    alert(Math.round((dvc_ratio + Number.EPSILON) * 100) / 100);
 }
 document.getElementById('button').addEventListener('click', alertDeviceRatio);
 
